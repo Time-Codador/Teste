a3e16b340a307529c992c114c7e919e4c655d8fb
ENZO KAWAKAMI MORETTI <enzo.moretti3@senaisp.edu.br>
2026-08-28 16:25:35 -0300

    Teste-Backend

--- src/main/java/com/senai/crud/ProdutoControler.java
+++ src/main/java/com/senai/crud/ProdutoControler.java
@@ -4,15 +4,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoControler {
 
-    @PostMapping
-    public ResponseEntity<Produtos> salvarProduto(@RequestBody Produtos produtoDs) {
+    List<Produtos> ps = new ArrayList<>();
 
+    @PostMapping
+    public ResponseEntity<?> salvarProduto(@RequestBody Produtos produtoDs) {
+        ps.add(produtoDs);
         System.out.println("Dispositivo móvel enviou: " + produtoDs.nome());
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(produtoDs);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ps);
     }
+
+
 }
+
